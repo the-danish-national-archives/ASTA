280970a454b1e6a8a1ad1395bd010f12bfc53ee3
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-02-15 15:27:03 +0100

    Event logic
    Added logic to handle button clicks from the html files..

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const electron = require('electron')
+
+document.body.addEventListener('click', (event) => {
+    var el = event.target;
+    if (el.id == "okBtn"){
+        var te = document.getElementById('path').value;
+        console.log(te);
+    }
+});
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1300,
@@ -21,7 +22,7 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-  
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
@@ -55,3 +56,15 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+function makeSingleInstance() {
+  if (process.mas) return
+
+  app.requestSingleInstanceLock()
+
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
