735bcfec5908cff776faf4412b48d488d66c1e10
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-02-21 12:06:41 +0100

    Created packager and installer
    (Windows only)
    Updated the project with the packages and libraries to create the installer and packages.
    Created the logic to handle the creation of the package and the executable.
    (Mac and Linux)
    Created scripts for packaing

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
-
+const setupEvents = require('./installers/setupEvents')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+if (setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
 
 function createWindow() {
   // Create the browser window.
