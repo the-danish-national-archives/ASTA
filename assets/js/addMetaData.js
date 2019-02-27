f3d1d8f9ae0fabaaa8c72ae63430d9a8f10ef35f
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-02-27 16:37:53 +0100

    Regex for meta data input
    Created some html and logic for the meta data regex .

--- assets/js/addMetaData.js
+++ assets/js/addMetaData.js
@@ -1,33 +1,70 @@
 window.Rigsarkiv = window.Rigsarkiv || {},
-function (n) {
-    const {ipcRenderer} = require('electron');
+    function (n) {
+        const { ipcRenderer } = require('electron');
+        const startNumberPattern = (/^([a-zA-ZæøåÆØÅ])([a-zA-ZæøåÆØÅ0-9]*)/)
+        const quotesPattern = /^"([a-zA-ZæøåÆØÅ0-9]*)"$/;
 
-    var settings = {
-        fileName: null,
-        fileDescr: null,
-        keyVar: null,
-        foreignFileName: null,
-        foreignKeyVarName: null,
-        foreignFileRefVar: null,
-        okBtn: null
-    }
-
-    var AddEvents = function () {
-        settings.okBtn.addEventListener('click', function(event){
-            console.log('file: ' + settings.fileName.value);
-        })
-    }
-
-    Rigsarkiv.MetaData = {
-        initialize: function (metadataFileName, metadataFileNameDescription, metadataKeyVariable, metadataForeignFileName, metadataForeignKeyVariableName, metadataReferenceVariable, metdataOkBtn){
-            settings.fileName = document.getElementById(metadataFileName);
-            settings.fileDescr = document.getElementById(metadataFileNameDescription);
-            settings.keyVar = document.getElementById(metadataKeyVariable);  
-            settings.foreignFileName = document.getElementById(metadataForeignFileName);
-            settings.foreignKeyVarName =  document.getElementById(metadataForeignKeyVariableName);
-            settings.foreignFileRefVar = document.getElementById(metadataReferenceVariable);
-            settings.okBtn = document.getElementById(metdataOkBtn);
-            AddEvents();
+        var settings = {
+            fileName: null,
+            fileDescr: null,
+            keyVar: null,
+            foreignFileName: null,
+            foreignKeyVarName: null,
+            foreignFileRefVar: null,
+            okBtn: null,
+            // Messages
+            fileNameReq: null,
+            numberFirst: null,
+            illegalChar: null
         }
-    }
-}(jQuery);
+
+        var Reset = function () {
+            settings.fileNameReq.hidden = true;
+            settings.numberFirst.hidden = true;
+        }
+
+        function ValidateFields() {
+            var validationOk = true;
+            if (startNumberPattern.test(settings.fileName.value) || quotesPattern.test(settings.fileName.value)){
+                console.log("valid");
+            } else {
+                console.log('error');
+            }
+
+
+            // if (settings.fileName.value === "") {
+            //     settings.fileNameReq.hidden = false;
+            // }
+            // if (startNumberPattern.test(settings.fileName.value)) {
+            //     settings.numberFirst.hidden =  false;
+            // }
+            // else if (!quotesPattern.test(settings.fileName.value)) {
+            //     settings.illegalChar.hidden = false;
+            // }
+
+        }
+
+        var AddEvents = function () {
+            settings.okBtn.addEventListener('click', function (event) {
+                Reset();
+                ValidateFields();
+            })
+        }
+
+        Rigsarkiv.MetaData = {
+            initialize: function (metadataFileName, metadataFileNameDescription, metadataKeyVariable, metadataForeignFileName, metadataForeignKeyVariableName, metadataReferenceVariable, metdataOkBtn,
+                inputFileNameRequired, inputNumberFirst, inputIllegalChar) {
+                settings.fileName = document.getElementById(metadataFileName);
+                settings.fileDescr = document.getElementById(metadataFileNameDescription);
+                settings.keyVar = document.getElementById(metadataKeyVariable);
+                settings.foreignFileName = document.getElementById(metadataForeignFileName);
+                settings.foreignKeyVarName = document.getElementById(metadataForeignKeyVariableName);
+                settings.foreignFileRefVar = document.getElementById(metadataReferenceVariable);
+                settings.okBtn = document.getElementById(metdataOkBtn);
+                settings.fileNameReq = document.getElementById(inputFileNameRequired);
+                settings.numberFirst = document.getElementById(inputNumberFirst);
+                settings.illegalChar = document.getElementById(inputIllegalChar);
+                AddEvents();
+            }
+        }
+    }(jQuery);
