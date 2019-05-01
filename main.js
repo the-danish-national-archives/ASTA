c238227c1a116164f08f7468eea49cad6e7ff13a
DDATEST\saraf <raf@sa.dk>
2019-05-01 11:36:40 +0200

    Test to see if I could edit, minor spell error fix.

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow() {
       {
         type: 'warning',
         cancelId: 1,
-        buttons: ['FORSÆT', 'FORTRYD'],
+        buttons: ['FORTSÆT', 'FORTRYD'],
         title: 'Programmet lukkes',
         message: 'Du er ved at lukke programmet ned. Er du sikker på at du vil afbryde programmet? OBS: Hvis programmet lukkes, mens du er i gang med at lave et dataudtræk med Hybris, vil dataudtrækket ikke blive færdiggjort og du skal skabe afleveringspakken igen helt forfra. Det du har udført i Hybris allerede vil blive gemt. OBS: Husk at Hybris skal forblive åben, samtidig med at du kører export scriptet i dit statistikprogram i TRIN 2 under et dataudtræk.'
      });
