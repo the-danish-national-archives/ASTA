6c1d450849ab1538824eb3fe3d595d61ab66ece6
Nawar Hajwal <nawar@proactive.dk>
2019-05-20 11:23:10 +0200

    H1007-7

--- electron/assets/js/hybris/contextdocuments.js
+++ electron/assets/js/hybris/contextdocuments.js
@@ -8,25 +8,64 @@ function (n) {
     Rigsarkiv.Hybris = Rigsarkiv.Hybris || {},
     function (n) {
         const { ipcRenderer } = require('electron');
-        const fs = require('fs');
+        const domParser = require('xmldom');
 
         //private data memebers
         var settings = {
             structureCallback: null,
-            IndecesPath: null,
-            IndecesPostfix: "Indices",
+            okBtn: null,
+            outputErrorSpn: null,
+            outputErrorText: null,
+            xmlDocuments: null
+        }
+
+         //output system error messages
+         var HandleError = function(err) {
+            console.log(`Error: ${err}`);
+            settings.outputErrorSpn.hidden = false;
+            settings.outputErrorSpn.innerHTML = settings.outputErrorText.format(err.message);            
+        }
+
+        // Render Documents control
+        var RenderDocuments = function(data) {
+            if(data != null && data.toString() != null && data.toString() !== "") {
+                settings.xmlDocuments = new domParser.DOMParser().parseFromString(data.toString());
+                for(var i = 0; i < settings.xmlDocuments.documentElement.childNodes.length;i++) {
+                    var node = settings.xmlDocuments.documentElement.childNodes[i];
+                    if(node.nodeName === "document" && node.childNodes != null) {
+                        
+                    }
+                }    
+            }
+            else {
+
+            }
+        }
+
+        //add Event Listener to HTML elmenets
+        var AddEvents = function () {
+            settings.okBtn.addEventListener('click', function (event) {
+            });
         }
         
         //Model interfaces functions
         Rigsarkiv.Hybris.ContextDocuments = {
-            initialize: function (structureCallback) {
+            initialize: function (structureCallback,outputErrorId,okId) {
                 settings.structureCallback = structureCallback;
+                settings.okBtn = document.getElementById(okId);
+                settings.outputErrorSpn =  document.getElementById(outputErrorId);
+                settings.outputErrorText = settings.outputErrorSpn.innerHTML;
+                AddEvents();
             },
             callback: function () {
                 return {
-                    load: function() {
-                        settings.IndecesPath = settings.structureCallback().deliveryPackagePath;
-                        settings.IndecesPath += (settings.IndecesPath.indexOf("\\") > -1) ? "\\{0}".format(settings.IndecesPostfix) : "/{0}".format(settings.IndecesPostfix);
+                    load: function(data) {
+                        try {
+                            RenderDocuments(data);
+                        }
+                        catch(err) {
+                            HandleError(err);
+                        } 
                     }
                 }
             }
