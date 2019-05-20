1fcb46be04084f93f88a54010ac2ecfaaecc785b
Nawar Hajwal <nawar@proactive.dk>
2019-05-20 12:02:43 +0200

    H1007-7

--- electron/assets/js/hybris/contextdocuments.js
+++ electron/assets/js/hybris/contextdocuments.js
@@ -16,7 +16,8 @@ function (n) {
             okBtn: null,
             outputErrorSpn: null,
             outputErrorText: null,
-            xmlDocuments: null
+            uploadsTbl: null,
+            documents: []
         }
 
          //output system error messages
@@ -26,16 +27,36 @@ function (n) {
             settings.outputErrorSpn.innerHTML = settings.outputErrorText.format(err.message);            
         }
 
+        //reset status & input fields
+        var Reset = function () {
+            settings.outputErrorSpn.hidden = true;
+            $("#{0} tr:not(:first-child)".format(settings.uploadsTbl.id)).remove();
+            settings.documents = [];
+        }
+
         // Render Documents control
         var RenderDocuments = function(data) {
             if(data != null && data.toString() != null && data.toString() !== "") {
-                settings.xmlDocuments = new domParser.DOMParser().parseFromString(data.toString());
-                for(var i = 0; i < settings.xmlDocuments.documentElement.childNodes.length;i++) {
-                    var node = settings.xmlDocuments.documentElement.childNodes[i];
+                var doc = new domParser.DOMParser().parseFromString(data.toString());
+                for(var i = 0; i < doc.documentElement.childNodes.length;i++) {
+                    var node = doc.documentElement.childNodes[i];
                     if(node.nodeName === "document" && node.childNodes != null) {
-                        
+                        var document = {"id":"", "title":"", "path":""};
+                        for(var j = 0; j < node.childNodes.length;j++) {
+                            if(node.childNodes[j].nodeName === "documentID") {
+                                document.id = node.childNodes[j].firstChild.data;
+                            }
+                            if(node.childNodes[j].nodeName === "documentTitle") {
+                                document.title = node.childNodes[j].firstChild.data;
+                            }
+                        }
+                        settings.documents.push(document);                         
                     }
-                }    
+                }
+                settings.documents.forEach(document => {
+                    $(settings.uploadsTbl).append("<tr><td>{0}</td><td>{1}</td><td><input type=\"text\" id=\"hybris-contextdocuments-document-{0}\" class=\"path\" text=\"{2}\"/></td><td><button id=\"hybris-indexfiles-contextdocuments-selectFile-{0}\">Browse</button></td></tr>".format(document.id,document.title,document.path));
+                });
+                    
             }
             else {
 
@@ -50,17 +71,19 @@ function (n) {
         
         //Model interfaces functions
         Rigsarkiv.Hybris.ContextDocuments = {
-            initialize: function (structureCallback,outputErrorId,okId) {
+            initialize: function (structureCallback,outputErrorId,okId,uploadsId) {
                 settings.structureCallback = structureCallback;
                 settings.okBtn = document.getElementById(okId);
                 settings.outputErrorSpn =  document.getElementById(outputErrorId);
                 settings.outputErrorText = settings.outputErrorSpn.innerHTML;
+                settings.uploadsTbl = document.getElementById(uploadsId);
                 AddEvents();
             },
             callback: function () {
                 return {
                     load: function(data) {
                         try {
+                            Reset();
                             RenderDocuments(data);
                         }
                         catch(err) {
