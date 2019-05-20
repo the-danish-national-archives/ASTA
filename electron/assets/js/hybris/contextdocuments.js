7f0372f7ba9a125c186ebb80de45dc1b6daf8158
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-05-20 15:29:45 +0100

    Updated error handling
    Updated the message given when the program encounters an file not found error.

--- electron/assets/js/hybris/contextdocuments.js
+++ electron/assets/js/hybris/contextdocuments.js
@@ -23,8 +23,15 @@ function (n) {
          //output system error messages
          var HandleError = function(err) {
             console.log(`Error: ${err}`);
+            var msg = ""
+            if (err.code === "ENOENT") {
+                msg = "Der er opstået en fejl i dannelsen af afleveringspakken. Genstart venligst programmet.";
+            }
+            else {
+                msg = err.message
+            }
             settings.outputErrorSpn.hidden = false;
-            settings.outputErrorSpn.innerHTML = settings.outputErrorText.format(err.message);            
+            settings.outputErrorSpn.innerHTML = settings.outputErrorText.format(msg);       
         }
 
         //reset status & input fields
