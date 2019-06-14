aa8d765aa7ed55fed930a6f6d7ed013339bd2ace
Nawar Hajwal <nawar@proactive.dk>
2019-06-14 23:15:05 +0200

    A1007-23

--- dotNET/Athena/MetaData.cs
+++ dotNET/Athena/MetaData.cs
@@ -17,11 +17,16 @@ namespace Rigsarkiv.Athena
         const string ColumnNode = "<column><name></name><columnID></columnID><type></type><typeOriginal></typeOriginal><nullable></nullable><description></description></column>";
         const string TableNode = "<table><name></name><folder></folder><description></description><columns></columns><primaryKey><name></name></primaryKey><foreignKeys></foreignKeys><rows></rows></table>";
         const string PrimaryKeyColumnNode = "<column></column>";
+        const string ForeignKeyNode = "<foreignKey><name></name><referencedTable></referencedTable><reference><column></column><referenced>Kode</referenced></reference></foreignKey>";
+        const string ReferencedTableNode = "<table><name></name><folder></folder><description></description><columns><column><name>Kode</name><columnID>c1</columnID><type></type><typeOriginal></typeOriginal><nullable>false</nullable><description>Kode</description></column><column><name>Kodeværdi</name><columnID>c2</columnID><type></type><typeOriginal></typeOriginal><nullable>false</nullable><description>Kodeværdi</description></column></columns><primaryKey><name></name><column>Kode</column></primaryKey><rows></rows></table>";
         const string IndicesPath = "{0}\\Indices";
         const string TableIndex = "tableIndex.xml";
         const string TableFolderPrefix = "table{0}";
         const string ColumnIDPrefix = "c{0}";
         const string PrimaryKeyPrefix = "PK_{0}";
+        const string ForeignKeyPrefix = "PK_{0}_{1}_{2}";
+        const string ReferencedTable = "{0}_{1}";
+        const string ReferencedTableDescription = "Kodeliste til tabel {0}";
         const string VarCharPrefix = "VARCHAR({0})";
         private dynamic _metadata = null;
         private XmlDocument _tableDocument = null;
@@ -110,18 +115,58 @@ namespace Rigsarkiv.Athena
         {
             var node = CreateNode(_tableDocument, tableNode.SelectSingleNode("columns"), ColumnNode);
             var columnName = variableInfo["name"].ToString();
+            var columnType = GetMappedType(variableInfo);
             node.SelectSingleNode("name").InnerText = columnName;
             node.SelectSingleNode("columnID").InnerText = string.Format(ColumnIDPrefix, index);
             node.SelectSingleNode("typeOriginal").InnerText = variableInfo["format"].ToString();
             node.SelectSingleNode("nullable").InnerText = variableInfo["nullable"].ToString().ToLower();
             node.SelectSingleNode("description").InnerText = variableInfo["description"].ToString();
-            node.SelectSingleNode("type").InnerText = GetMappedType(variableInfo);
+            node.SelectSingleNode("type").InnerText = columnType;
             if((bool)variableInfo["isKey"])
             {
                 _logManager.Add(new LogEntity() { Level = LogLevel.Info, Section = _logSection, Message = string.Format("Add key: {0} ", columnName) });
                 var primaryKeyNode = CreateNode(_tableDocument, tableNode.SelectSingleNode("primaryKey"), PrimaryKeyColumnNode);
                 primaryKeyNode.InnerText = columnName;
             }
+            if (variableInfo["codeListKey"] != null && !string.IsNullOrEmpty(variableInfo["codeListKey"].ToString()))
+            {
+                AddReferencedTable(variableInfo, tableNode, index, columnType);
+            }
+        }
+        
+        private void AddReferencedTable(Dictionary<string, object> variableInfo, XmlNode tableNode, int index, string columnType)
+        {
+            _tablesCounter++;
+            var tableName = tableNode.SelectSingleNode("name").InnerText;
+            var codeListKey = variableInfo["codeListKey"].ToString();
+            var refTableName = string.Format(ReferencedTable, tableName, codeListKey);
+            _logManager.Add(new LogEntity() { Level = LogLevel.Info, Section = _logSection, Message = string.Format("Add referenced Table: {0} ", refTableName) });
+
+            var node = CreateNode(_tableDocument, tableNode.SelectSingleNode("foreignKeys"), ForeignKeyNode);
+            node.SelectSingleNode("name").InnerText = string.Format(ForeignKeyPrefix, tableName, codeListKey, index);
+            node.SelectSingleNode("referencedTable").InnerText = refTableName;
+            node.SelectSingleNode("reference/column").InnerText = variableInfo["name"].ToString();
+           
+            var refTableNode = CreateNode(_tableDocument, tableNode.ParentNode, ReferencedTableNode);
+            refTableNode.SelectSingleNode("name").InnerText = refTableName;
+            refTableNode.SelectSingleNode("folder").InnerText = string.Format(TableFolderPrefix, _tablesCounter);
+            refTableNode.SelectSingleNode("description").InnerText = string.Format(ReferencedTableDescription, tableName);
+            refTableNode.SelectSingleNode("primaryKey/name").InnerText = string.Format(PrimaryKeyPrefix, refTableName);
+            var columnNodes = refTableNode.SelectNodes("columns/column");
+            columnNodes[0].SelectSingleNode("type").InnerText = columnType;
+            columnNodes[1].SelectSingleNode("type").InnerText = ParseOptions((object[])variableInfo["options"], refTableNode);
+        }
+
+        private string ParseOptions(object[] options, XmlNode refTableNode)
+        {
+            var result = 0;
+            refTableNode.SelectSingleNode("rows").InnerText = options.Length.ToString();
+            foreach (var option in options)
+            {
+                var length = ((Dictionary<string, object>)option)["description"].ToString().Length;
+                if(length > result) { result = length; }
+            }
+            return string.Format(VarCharPrefix, result);
         }
 
         private string GetMappedType(Dictionary<string, object> variableInfo)
