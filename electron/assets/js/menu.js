4793c83eae535d66e812bc679144a1d58053925e
Nawar Hajwal <nawar@proactive.dk>
2019-07-29 20:05:35 +0200

    S1007-4

--- electron/assets/js/menu.js
+++ electron/assets/js/menu.js
@@ -6,12 +6,19 @@
 window.navigation = window.navigation || {},
     function (n) {
         const {getCurrentWindow, globalShortcut} = require('electron').remote;
+        const fs = require('fs');
+        const path = require('path');
+        const os = require('os');
+        const { spawn } = require('child_process');
         navigation.menu = {
             constants: {
                 sectionTemplate: '.section-template',
                 contentContainer: '#content',
                 startSectionMenuItem: "#welcome-menu",
-                startSection: "#welcome"
+                startSection: "#welcome",
+                scriptPath: "./assets/scripts/{0}",
+                resourceWinPath: "resources\\{0}",
+                converterFileName: "StyxForm.exe"
             },
 
             importSectionsToDOM: function () {
@@ -60,12 +67,33 @@ window.navigation = window.navigation || {},
         };
 
         n(function () {
+            var outputErrorSpn = document.getElementById("menu-output-Error");
+            var outputErrorText = outputErrorSpn.innerHTML;
             Rigsarkiv.Rights.initialize("menu-output-Error");
             navigation.menu.init();
             document.getElementById("menu-reload").addEventListener('click', function (event) {
                 getCurrentWindow().reload();
             });
             styxLink = document.getElementById("styx-menu");
+            styxLink.addEventListener('click', function (event) {
+                var converterFilePath = navigation.menu.constants.scriptPath.format(navigation.menu.constants.converterFileName);        
+                if(!fs.existsSync(converterFilePath)) {
+                    var rootPath = null;
+                    if(os.platform() == "win32") {
+                        rootPath = path.join('./');
+                        converterFilePath = path.join(rootPath,navigation.menu.constants.resourceWinPath.format(navigation.menu.constants.converterFileName));
+                    }
+                    if(os.platform() == "darwin") {
+                        var folders =  __dirname.split("/");
+                        rootPath = folders.slice(0,folders.length - 3).join("/");
+                        converterFilePath = "{0}/{1}".format(rootPath,navigation.menu.constants.converterFileName);
+                    }
+                }   
+                var converter = spawn(converterFilePath);
+                converter.stdout.on('data', (data) => console.log(`stdout: ${data}`));                  
+                converter.stderr.on('data', (data) => (new Error(data).Handle(outputErrorSpn,outputErrorText,"Rigsarkiv.Nemesis.Data.AddEvents")));
+                converter.on('close', (code) => console.log(`converter process exited with code ${code}`));
+            });
             $(styxLink).hide();
             if(Rigsarkiv.Rights.callback().isAdmin) {
                 $(styxLink).show();
