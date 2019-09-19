4f28940d946488e6a0e7df42ba4f549c65b412a9
Nawar Hajwal <nawar@proactive.dk>
2019-09-19 11:06:29 +0200

    ensure links

--- electron/assets/js/links.js
+++ electron/assets/js/links.js
@@ -30,7 +30,7 @@ window.Rigsarkiv = window.Rigsarkiv || {},
             if(os.platform() == "darwin") {                
                 var folders =  __dirname.split("/");
                 rootPath = folders.slice(0,folders.length - 1).join("/");
-                result = "{0}\\documents\\{1}".format(rootPath,fileName);
+                result = "{0}/documents/{1}".format(rootPath,fileName);
                 if(!fs.existsSync(settings.documentPath.format(fileName))) {
                     rootPath = folders.slice(0,folders.length - 3).join("/");
                     result = "{0}/{1}".format(rootPath,fileName);
