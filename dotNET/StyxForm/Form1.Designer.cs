8ee9ee9cfa0faeae92e6157e01ece76600cf22b1
blynnerup <brian.lynnerup.pedersen@gmail.com>
2019-09-19 10:51:20 +0200

    Updated the Styx interface

--- dotNET/StyxForm/Form1.Designer.cs
+++ dotNET/StyxForm/Form1.Designer.cs
@@ -54,9 +54,13 @@
             // 
             // outputRichTextBox
             // 
-            this.outputRichTextBox.Location = new System.Drawing.Point(30, 365);
+            this.outputRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputRichTextBox.Location = new System.Drawing.Point(19, 244);
+            this.outputRichTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.outputRichTextBox.Name = "outputRichTextBox";
-            this.outputRichTextBox.Size = new System.Drawing.Size(964, 227);
+            this.outputRichTextBox.Size = new System.Drawing.Size(642, 158);
             this.outputRichTextBox.TabIndex = 0;
             this.outputRichTextBox.Text = "";
             // 
@@ -64,65 +68,80 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 9);
+            this.label4.Location = new System.Drawing.Point(15, 6);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(294, 29);
+            this.label4.Size = new System.Drawing.Size(222, 20);
             this.label4.TabIndex = 16;
-            this.label4.Text = "Konverter til mellemformat";
+            this.label4.Text = "Konverter til udleveringsformat";
             // 
             // sipTextBox
             // 
-            this.sipTextBox.Location = new System.Drawing.Point(27, 217);
+            this.sipTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sipTextBox.Location = new System.Drawing.Point(18, 141);
+            this.sipTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sipTextBox.Name = "sipTextBox";
-            this.sipTextBox.Size = new System.Drawing.Size(831, 26);
+            this.sipTextBox.Size = new System.Drawing.Size(566, 20);
             this.sipTextBox.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 194);
+            this.label3.Location = new System.Drawing.Point(15, 126);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(294, 20);
+            this.label3.Size = new System.Drawing.Size(197, 13);
             this.label3.TabIndex = 21;
             this.label3.Text = "Vælg DIP destination (drev eller mappe):";
             // 
             // sipNameTextBox
             // 
-            this.sipNameTextBox.Location = new System.Drawing.Point(27, 144);
+            this.sipNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sipNameTextBox.Location = new System.Drawing.Point(18, 94);
+            this.sipNameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sipNameTextBox.Name = "sipNameTextBox";
-            this.sipNameTextBox.Size = new System.Drawing.Size(831, 26);
+            this.sipNameTextBox.Size = new System.Drawing.Size(566, 20);
             this.sipNameTextBox.TabIndex = 20;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 121);
+            this.label2.Location = new System.Drawing.Point(17, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.Size = new System.Drawing.Size(167, 13);
             this.label2.TabIndex = 19;
-            this.label2.Text = "Angiv DIP ID:";
+            this.label2.Text = "Angiv DIP-ID, fx. DIP.12345 (DIP)";
             // 
             // aipTextBox
             // 
-            this.aipTextBox.Location = new System.Drawing.Point(27, 74);
+            this.aipTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.aipTextBox.Location = new System.Drawing.Point(18, 48);
+            this.aipTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.aipTextBox.Name = "aipTextBox";
-            this.aipTextBox.Size = new System.Drawing.Size(831, 26);
+            this.aipTextBox.Size = new System.Drawing.Size(566, 20);
             this.aipTextBox.TabIndex = 18;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 51);
+            this.label1.Location = new System.Drawing.Point(15, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 20);
+            this.label1.Size = new System.Drawing.Size(256, 13);
             this.label1.TabIndex = 17;
-            this.label1.Text = "Vælg arkiveringsversion (AIP):";
+            this.label1.Text = "Vælg arkiveringsversion - fx. AVID.SA.12345.1 (AIP):";
             // 
             // sipButton
             // 
-            this.sipButton.Location = new System.Drawing.Point(898, 211);
+            this.sipButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sipButton.Location = new System.Drawing.Point(588, 141);
+            this.sipButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sipButton.Name = "sipButton";
-            this.sipButton.Size = new System.Drawing.Size(93, 32);
+            this.sipButton.Size = new System.Drawing.Size(73, 21);
             this.sipButton.TabIndex = 24;
             this.sipButton.Text = "Browse";
             this.sipButton.UseVisualStyleBackColor = true;
@@ -130,9 +149,11 @@
             // 
             // aipButton
             // 
-            this.aipButton.Location = new System.Drawing.Point(898, 74);
+            this.aipButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.aipButton.Location = new System.Drawing.Point(588, 48);
+            this.aipButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.aipButton.Name = "aipButton";
-            this.aipButton.Size = new System.Drawing.Size(96, 32);
+            this.aipButton.Size = new System.Drawing.Size(73, 21);
             this.aipButton.TabIndex = 23;
             this.aipButton.Text = "Browse";
             this.aipButton.UseVisualStyleBackColor = true;
@@ -141,9 +162,10 @@
             // logButton
             // 
             this.logButton.Enabled = false;
-            this.logButton.Location = new System.Drawing.Point(30, 318);
+            this.logButton.Location = new System.Drawing.Point(18, 219);
+            this.logButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.logButton.Name = "logButton";
-            this.logButton.Size = new System.Drawing.Size(75, 32);
+            this.logButton.Size = new System.Drawing.Size(73, 21);
             this.logButton.TabIndex = 26;
             this.logButton.Text = "Vis Log";
             this.logButton.UseVisualStyleBackColor = true;
@@ -151,9 +173,11 @@
             // 
             // convertButton
             // 
-            this.convertButton.Location = new System.Drawing.Point(855, 318);
+            this.convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertButton.Location = new System.Drawing.Point(588, 219);
+            this.convertButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.convertButton.Name = "convertButton";
-            this.convertButton.Size = new System.Drawing.Size(136, 32);
+            this.convertButton.Size = new System.Drawing.Size(73, 21);
             this.convertButton.TabIndex = 25;
             this.convertButton.Text = "Konverter til DIP";
             this.convertButton.UseVisualStyleBackColor = true;
@@ -174,26 +198,29 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 268);
+            this.label5.Location = new System.Drawing.Point(15, 180);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(155, 20);
+            this.label5.Size = new System.Drawing.Size(102, 13);
             this.label5.TabIndex = 27;
             this.label5.Text = "Vælg statistikformat:";
             // 
             // scriptTypeComboBox
             // 
             this.scriptTypeComboBox.FormattingEnabled = true;
-            this.scriptTypeComboBox.Location = new System.Drawing.Point(195, 265);
+            this.scriptTypeComboBox.Location = new System.Drawing.Point(118, 177);
+            this.scriptTypeComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.scriptTypeComboBox.Name = "scriptTypeComboBox";
-            this.scriptTypeComboBox.Size = new System.Drawing.Size(121, 28);
+            this.scriptTypeComboBox.Size = new System.Drawing.Size(73, 21);
             this.scriptTypeComboBox.TabIndex = 28;
             // 
             // reportButton
             // 
             this.reportButton.Enabled = false;
-            this.reportButton.Location = new System.Drawing.Point(132, 318);
+            this.reportButton.Location = new System.Drawing.Point(118, 219);
+            this.reportButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.reportButton.Name = "reportButton";
-            this.reportButton.Size = new System.Drawing.Size(111, 32);
+            this.reportButton.Size = new System.Drawing.Size(73, 21);
             this.reportButton.TabIndex = 29;
             this.reportButton.Text = "Vis Rapport";
             this.reportButton.UseVisualStyleBackColor = true;
@@ -201,9 +228,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1005, 604);
+            this.ClientSize = new System.Drawing.Size(684, 413);
             this.Controls.Add(this.reportButton);
             this.Controls.Add(this.scriptTypeComboBox);
             this.Controls.Add(this.label5);
@@ -219,6 +246,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.outputRichTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "Form1";
             this.Text = "ASTA";
             this.Shown += new System.EventHandler(this.Form1_Shown);
