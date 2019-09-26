edc51f86a40fd960b62011d9283bc9b2e3e59589
Nawar Hajwal <nawar@proactive.dk>
2019-09-26 23:22:46 +0200

    H1007-155

--- electron/assets/js/menu.js
+++ electron/assets/js/menu.js
@@ -83,6 +83,10 @@ window.navigation = window.navigation || {},
             var languageCallback = Rigsarkiv.Language.callback();
             languageCallback.setLanguage(Rigsarkiv.Profile.callback().data.lcid);
             Rigsarkiv.Links.callback().updateLinks(["instructions-link1","instructions-link2","instructions-link3","instructions-link4","instructions-link5","instructions-link6","instructions-link7","instructions-link8","instructions-link9","instructions-link10","instructions-link11","instructions-link12"]);
+            //TODO :remove
+            var profileLink  = document.getElementById("menu-profile");
+            $(profileLink).hide();
+            
             document.getElementById("menu-reload").addEventListener('click', function (event) {
                 ipcRenderer.send('open-confirm-dialog','menu-reload',"Program genstart","Du er ved at genstarte programmet. Er du sikker?","GENSTART","FORTRYD");
             });
