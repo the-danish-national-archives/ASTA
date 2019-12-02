26914a20bb7dcf796ae933e52093fb632bd6adf7
Nawar Hajwal <nawar@proactive.dk>
2019-12-02 15:20:19 +0100

    N1007-134

--- electron/assets/js/hybris/backup.js
+++ electron/assets/js/hybris/backup.js
@@ -7,7 +7,10 @@ window.Rigsarkiv = window.Rigsarkiv || {},
 function (n) {
     Rigsarkiv.Hybris = Rigsarkiv.Hybris || {},
     function (n){
-        const {ipcRenderer} = require('electron')
+        const {ipcRenderer} = require('electron');
+        const {shell} = require('electron');
+        const path = require('path');
+        const os = require('os');
         const fs = require('fs');
         const fse = require('fs-extra');
 
@@ -24,7 +27,14 @@ function (n) {
             indexfilesTab: null,
             hasSelected: false,
             checkboxPrefixId: "hybris-backup-folder-",
+            filePostfix: "{0}_ASTA_backup.html",
             pathPostfix: "{0}ASTA_udtrækslog_{1}",
+            templateFileName: "backup.html",
+            scriptPath: "./assets/scripts/{0}",
+            resourceWinPath: "resources\\{0}",
+            filePath: null,
+            folderPath: null,
+            logs: []
         }
 
         //reset status & input fields
@@ -53,10 +63,10 @@ function (n) {
                 var selectedFolderPath = Rigsarkiv.Hybris.Structure.callback().deliveryPackagePath;
                 var folders = selectedFolderPath.getFolders();
                 var fileName = folders[folders.length - 1];
-                settings.filePath = settings.pathPostfix.format(selectedFolderPath.substring(0,selectedFolderPath.lastIndexOf((selectedFolderPath.indexOf("\\") > -1) ? "\\" : "/") + 1),fileName);
-                if(!fs.existsSync(settings.filePath)) {                        
-                    console.logInfo(`Create log folder: ${settings.filePath}`,"Rigsarkiv.Hybris.Backup.EnsurePath");                     
-                    fs.mkdirSync(settings.filePath);
+                settings.folderPath = settings.pathPostfix.format(selectedFolderPath.substring(0,selectedFolderPath.lastIndexOf((selectedFolderPath.indexOf("\\") > -1) ? "\\" : "/") + 1),fileName);
+                if(!fs.existsSync(settings.folderPath)) {                        
+                    console.logInfo(`Create log folder: ${settings.folderPath}`,"Rigsarkiv.Hybris.Backup.EnsurePath");                     
+                    fs.mkdirSync(settings.folderPath);
                 }
             }
             catch(err) {
@@ -65,7 +75,7 @@ function (n) {
         }
 
         //copy backups
-        var EnsureData = function() {
+        var EnsureStructure = function() {
             EnsurePath();
             try 
             {
@@ -73,15 +83,50 @@ function (n) {
                 $("input:checkbox[id^=" + settings.checkboxPrefixId + "]").each(function(index) {
                     if(this.checked) {
                         var folder = folders[index];
-                        console.logInfo(`backup folder: ${folder.path}`,"Rigsarkiv.Hybris.Backup.EnsureData");
-                        var destPath = settings.filePath.indexOf("\\") > -1 ? "{0}\\{1}".format(settings.filePath,folder.name) : "{0}/{1}".format(settings.filePath,folder.name);
+                        console.logInfo(`backup folder: ${folder.path}`,"Rigsarkiv.Hybris.Backup.EnsureStructure");
+                        var destPath = settings.folderPath.indexOf("\\") > -1 ? "{0}\\{1}".format(settings.folderPath,folder.name) : "{0}/{1}".format(settings.folderPath,folder.name);
                         fse.copySync(folder.path,destPath); 
                     }
                   });
             }
             catch(err) {
-                err.Handle(settings.outputErrorSpn,settings.outputErrorText,"Rigsarkiv.Hybris.Backup.EnsureData");
+                err.Handle(settings.outputErrorSpn,settings.outputErrorText,"Rigsarkiv.Hybris.Backup.EnsureStructure");
             } 
+        }
+
+        //commit print data
+        var EnsureData = function() {
+            var data = fs.readFileSync(settings.filePath);        
+            var folders = Rigsarkiv.Hybris.Structure.callback().deliveryPackagePath.getFolders();
+            var folderName = folders[folders.length - 1];
+            var title = Rigsarkiv.Language.callback().getValue("hybris-backup-template-Title");
+            var foldersList = Rigsarkiv.Language.callback().getValue("hybris-backup-template-foldersList-H3");
+            var th1 = Rigsarkiv.Language.callback().getValue("hybris-backup-template-foldersList-TH1");
+            var th2 = Rigsarkiv.Language.callback().getValue("hybris-backup-template-foldersList-TH2");
+            var th3 = Rigsarkiv.Language.callback().getValue("hybris-backup-template-foldersList-TH3");
+            var updatedData = data.toString().format(folderName,settings.logs.join("\r\n"),title,foldersList,th1,th2,th3);
+            fs.writeFileSync(settings.filePath, updatedData);                         
+            settings.logs = [];                               
+        }
+
+        //copy HTML template file to parent folder of selected Delivery Package folder
+        var CopyFile = function() {
+            var filePath = settings.scriptPath.format(settings.templateFileName);        
+            if(!fs.existsSync(filePath)) {
+                var rootPath = null;
+                if(os.platform() == "win32") {
+                    rootPath = path.join('./');
+                    filePath = path.join(rootPath,settings.resourceWinPath.format(settings.templateFileName));
+                }
+                if(os.platform() == "darwin") {
+                    var folders =  __dirname.split("/");
+                    rootPath = folders.slice(0,folders.length - 3).join("/");
+                    filePath = "{0}/{1}".format(rootPath,settings.templateFileName);
+                }
+            }        
+            console.logInfo(`copy ${settings.templateFileName} file to: ${settings.filePath}`,"Rigsarkiv.Hybris.Backup.CopyFile");
+            fs.copyFileSync(filePath, settings.filePath);
+            EnsureData();        
         }
 
         //enable/diable waiting spinner
@@ -103,7 +148,7 @@ function (n) {
                 }
                 else {             
                     UpdateSpinner(settings.spinnerClass); 
-                    EnsureData();
+                    EnsureStructure();
                     UpdateSpinner("");
                     settings.indexfilesTab.click();
                 }
@@ -120,6 +165,17 @@ function (n) {
             });
             settings.printBtn.addEventListener('click', function (event) {
                 EnsurePath();
+                var folders = Rigsarkiv.Hybris.Structure.callback().deliveryPackagePath.getFolders();
+                var fileName = folders[folders.length - 1];
+                settings.filePath = settings.folderPath.indexOf("\\") > -1 ? "{0}\\{1}".format(settings.folderPath,settings.filePostfix.format(fileName)) : "{0}/{1}".format(settings.folderPath,settings.filePostfix.format(fileName));
+                $("input:checkbox[id^=" + settings.checkboxPrefixId + "]").each(function(index) {
+                    if(this.checked) {  
+                        var folder = Rigsarkiv.Hybris.Base.callback().backup[index];
+                        settings.logs.push("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>".format(folder.path,folder.name,folder.size));
+                     }
+                }); 
+                CopyFile();
+                shell.openItem(settings.filePath);
             });
          }
 
