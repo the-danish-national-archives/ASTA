e8185b9a5f4616b2ac2bbb316fe554e9946449af
Rune <rhr@sa.dk>
2020-06-18 11:09:35 +0200

    updated more copyright years to 2020

--- poc/342 til 1007 POC/342_til_1007_migrering C#/342_til_1007_Vis_og_test/Properties/AssemblyInfo.cs
+++ poc/342 til 1007 POC/342_til_1007_migrering C#/342_til_1007_Vis_og_test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("342_til_1007_vis_test_konverter")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
