cd66014ddeb60e13842ad1052bf1d943add632ca
Kim Nordskov Adelhardt <kna@sa.dk>
2020-08-31 14:19:16 +0200

    New build script

--- BuildAsta.py
+++ BuildAsta.py
@@ -1,16 +1,103 @@
+#!/usr/bin/python
+
+import sys, getopt
 import os.path
 import platform
 import subprocess
 import time
 
-# Constants
-MIN_NODE_VERSION = 12
-MIN_NPM_VERSION = 6
-"""Edit the path to your location of MSBUILD"""
-MSBUILD_PATH = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\MSBuild\\Current\\Bin\\msbuild.exe'
-PATH_TO_ATHENA: str = './dotNET/Athena.sln'
-PATH_TO_STYX = './dotNET/Styx.sln'
-PATH_TO_ASSETS = './electron/assets/scripts/'
+
+class Settings:
+    def __init__(self,
+                 min_node_version=12,
+                 min_npm_version=6,
+                 msbuild_path='C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\msbuild.exe',
+                 nuget_path='nuget.exe',
+                 path_to_athena='./dotNET/Athena.sln',
+                 path_to_styx='./dotNET/Styx.sln',
+                 path_to_assets='./electron/assets/scripts/'):
+        """default values are set to fit Jenkins"""
+        self.min_nodejs_version = min_node_version
+        self.min_npm_version = min_npm_version
+        self.msbuild_path = msbuild_path
+        self.nuget_path = nuget_path
+        self.path_to_athena = path_to_athena
+        self.path_to_styx = path_to_styx
+        self.path_to_assets = path_to_assets
+
+    @property
+    def msbuild_path(self):
+        return self._msbuild_path
+
+    @msbuild_path.setter
+    def msbuild_path(self, value):
+        # if not os.path.exists(value):
+        #     raise ValueError("msbuild_path have to exists")
+        self._msbuild_path = value
+
+    @property
+    def path_to_styx(self):
+        return self._path_to_styx
+
+    @path_to_styx.setter
+    def path_to_styx(self, value):
+        # if not os.path.exists(value):
+        #     raise ValueError(" Path to styx solution have to exists")
+        self._path_to_styx = value
+
+    @property
+    def path_to_athena(self):
+        return self._path_to_athena
+
+    @property
+    def path_to_assets(self):
+        return self._path_to_assets
+
+    @path_to_assets.setter
+    def path_to_assets(self, value):
+        if not os.path.exists(value):
+            raise ValueError(" Path to assets have to exists")
+        self._path_to_assets = value
+
+    @path_to_athena.setter
+    def path_to_athena(self, value):
+        if not os.path.exists(value):
+            raise ValueError(" Path to Athena solution have to exists")
+        self._path_to_athena = value
+
+    @property
+    def min_nodejs_version(self):
+        return self._min_nodejs_version
+
+    @min_nodejs_version.setter
+    def min_nodejs_version(self, value):
+        if value < 12:  # Current version date 20200826
+            raise ValueError("min_nodejs_version have to be 12 or higher")
+        self._min_nodejs_version = value
+
+    @property
+    def min_npm_version(self):
+        return self._min_npm_version
+
+    @min_npm_version.setter
+    def min_npm_version(self, value):
+        if value < 6:  # Current version date 20200826
+            raise ValueError("min_npm_version have to be 6 or higher")
+        self._min_npm_version = value
+
+    @property
+    def nuget_path(self):
+        return self._nuget_path
+
+    @nuget_path.setter
+    def nuget_path(self, value):
+        # if not os.path.exists(value):
+        #     raise ValueError("nuget_path have to exists")
+        self._nuget_path = value
+
+
+settings = Settings()
+
 
 def verify_prerequisites(current_os):
     # Function checking the prerequisites
@@ -18,46 +105,56 @@ def verify_prerequisites(current_os):
     clear_to_go_ahead = True
 
     if current_os == "Windows":
-        if not os.path.exists(MSBUILD_PATH):
-            print(MSBUILD_PATH + ' mangler.')
+        if not os.path.exists(settings.msbuild_path):
+            print(settings.msbuild_path + ' mangler.')
             clear_to_go_ahead = False
 
-        if not os.path.isfile(PATH_TO_STYX):
-            print(os.getcwd() + PATH_TO_STYX + ' mangler')
+        if not os.path.isfile(settings.path_to_styx):
+            print(os.getcwd() + settings.path_to_styx + ' mangler')
             clear_to_go_ahead = False
 
-        if not os.path.isfile(PATH_TO_ATHENA):
-            print(os.getcwd() + PATH_TO_ATHENA + ' mangler')
+        if not os.path.isfile(settings.path_to_athena):
+            print(os.getcwd() + settings.path_to_athena + ' mangler')
+            clear_to_go_ahead = False
+
+        if not os.path.isfile(settings.nuget_path):
+            print(f'{settings.nuget_path} mangler')
             clear_to_go_ahead = False
 
     node_version = int(subprocess.run('node --version', shell=True, capture_output=True, text=True).stdout[1:3])
-    if not (node_version >= MIN_NODE_VERSION):
+    if not (node_version >= settings.min_nodejs_version):
         print(f'node {str(node_version)} mangler eller skal opdateres')
         clear_to_go_ahead = False
 
     npm_version = int(subprocess.run('npm --version', shell=True, capture_output=True, text=True).stdout[0:1])
-    if not (node_version >= MIN_NODE_VERSION):
-        print(f'npm {npm_version} mangler eller skal opdateres')
+    if not (npm_version >= settings.min_npm_version):
+        print(f'npm {settings.min_npm_version} mangler eller skal opdateres')
         clear_to_go_ahead = False
 
     return clear_to_go_ahead
 
 
+def build_dotnet(cmds):
+    for x in cmds:
+        print(x)
+        subprocess.run(x, shell=True)
+
+
 def build_athena():
     # Function to build the Release version of Athena (only on windows)
     print("Building Athena..")
-    cmd = f'"{MSBUILD_PATH}" /t:Build /p:Configuration=Release {PATH_TO_ATHENA}'
-    subprocess.run(cmd, shell=True)
-    print("last modified: %s" % time.ctime(os.path.getmtime(PATH_TO_ASSETS + "Athena.dll")))
+    build_dotnet({f'"{settings.nuget_path}" restore {settings.path_to_athena}',
+                  f'"{settings.msbuild_path}" /t:Build /p:Configuration=Release {settings.path_to_athena}'})
+    print("last modified: %s" % time.ctime(os.path.getmtime(settings.path_to_assets + "Athena.dll")))
 
 
 def build_styx():
     # Function to build the Release version of Styx (only on windows)
     print("Building Styx..")
-    cmd = f'"{MSBUILD_PATH}" /t:Build /p:Configuration=Release {PATH_TO_STYX}'
-    print(cmd)
-    subprocess.run(cmd, shell=True)
-    print("last modified: %s" % time.ctime(os.path.getmtime(PATH_TO_ASSETS + "Styx.dll")))
+    build_dotnet(
+        {f'{settings.nuget_path} restore {settings.path_to_styx}',
+         f'"{settings.msbuild_path}" /t:Build /p:Configuration=Release {settings.path_to_styx}'})
+    print("last modified: %s" % time.ctime(os.path.getmtime(settings.path_to_assets + "Styx.dll")))
 
 
 def build_asta(current_os):
@@ -98,17 +195,67 @@ def detect_platform() -> str:
     return str(platform.system())
 
 
-def main():
+def config_kna():
+    settings.msbuild_path = 'D:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\MSBuild\\Current\\Bin\\MSBuild.exe'
+    settings.nuget_path = 'C:\\FastRepos\\Asta\\electron\\node_modules\\electron-winstaller\\vendor\\nuget.exe'
+
+
+def config_tkn():
+    settings.nuget_path = ''
+    settings.msbuild_path = ''
+
+
+def config_rhr():
+    settings.nuget_path = ''
+    settings.msbuild_path = ''
+
+
+def setup_config(argv):
+    # Function for configuring the settings object
+    # if there is a parameter of type user verify its a known user
+    # and set it up accordingly by calling the matching config-function
+    legal_users = {'kna', 'tkn', 'rhr', 'Jenkins'}
+    user = ''
+    try:
+        opts, args = getopt.getopt(argv, "h:u:", ["user="])
+    except getopt.GetoptError:
+        print('BuildAsta.py -u <user>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('BuildAsta.py -u <user>')
+            print('Default user is Jenkins')
+            print('Legal users are:')
+            for p in legal_users:
+                print(f'\t{p}')
+            sys.exit()
+        elif opt in ("-u", "--user"):
+            user = arg.lower()
+
+    if user not in legal_users:
+        user = 'jenkins'
+        print('Legal user not found - moving forward with "Jenkins" as user')
+    print(f'user setting is "{user}"\n')
+    if user == 'kna':
+        config_kna()
+    if user == 'rhr':
+        config_rhr()
+    if user == 'tkn':
+        config_tkn()
+
+
+def main(argv):
     # Script to build all elements for Asta
     # Depending on the underlying platform different configurations is used
     tic = time.perf_counter()  # Start timer
+
+    setup_config(argv)
     print("Starting build artefacts...")
     current_os = detect_platform()
 
     print(f"\nPlatform identified as '{current_os}'\n")
-
     if verify_prerequisites(current_os):
-        if os == 'Windows':
+        if current_os == 'Windows':
             build_styx()
             build_athena()
 
@@ -118,4 +265,4 @@ def main():
     print(f"\nThe builds was DONE in {toc - tic:0.4f} seconds")  # stop timer and print
 
 
-main()
+main(sys.argv[1:])
